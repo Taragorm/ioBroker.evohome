d9ffbf2fbd8f467fbe5239aac215dc3d492c3e6a
Taragorm <taragorm@zoho.eu>
2020-06-14 16:31:50 +0100

    stringify

--- main.js
+++ main.js
@@ -263,6 +263,33 @@ riable', {
         }
     }
     //----------------------------------------------------------------------------------------------
+	function stringify(obj, replacer, spaces, cycleReplacer) {
+	  return JSON.stringify(obj, serializer(replacer, cycleReplacer), spaces)
+	}
+	
+    //----------------------------------------------------------------------------------------------
+	function serializer(replacer, cycleReplacer) {
+	  var stack = [], keys = []
+	
+	  if (cycleReplacer == null) cycleReplacer = function(key, value) {
+	    if (stack[0] === value) return "[Circular ~]"
+	    return "[Circular ~." + keys.slice(0, stack.indexOf(value)).join(".") + "]"
+	  }
+	
+	  return function(key, value) {
+	    if (stack.length > 0) {
+	      var thisPos = stack.indexOf(this)
+	      ~thisPos ? stack.splice(thisPos + 1) : stack.push(this)
+	      ~thisPos ? keys.splice(thisPos, Infinity, key) : keys.push(key)
+	      if (~stack.indexOf(value)) value = cycleReplacer.call(this, key, value)
+	    }
+	    else stack.push(value)
+	
+	    return replacer == null ? value : replacer.call(this, key, value)
+	  }
+	}    
+    //----------------------------------------------------------------------------------------------
+    
     /**
      * Move the evo data to iob
      * @param {boolean} quick   Don't get schedules
@@ -289,8 +316,8 @@ riable', {
                         ss("setpointMode");
                         ss("faults");
 
-                        this.setState(sensor+"zone", { val: JSON.stringify(st), ack: true});
-                        this.setState(sensor+"schedule", { val: JSON.stringify(zn.$schedule), ack: true});
+                        this.setState(sensor+"zone", { val: stringify(st), ack: true});
+                        this.setState(sensor+"schedule", { val: stringify(zn.$schedule), ack: true});
                     }
 
                     if(gsid) { 
